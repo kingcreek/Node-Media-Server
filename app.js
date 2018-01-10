6350c84f916cbeeb65efa3796dbef003acb7c3df
kingcreek <israel_launion@hotmail.com>
2018-01-10 08:08:11 +0100

    j

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const NodeMediaServer = require('./node_media_server');
 
 const config = {
   rtmp: {
-    port: 1935,
+    port: process.env.PORT || 1935,
     chunk_size: 60000,
     gop_cache: true,
     ping: 60,
     ping_timeout: 30
   },
   http: {
-    port: 8000,
+    port: process.env.PORT || 8000,
     allow_origin: '*'
   },
   https: {
-    port: 8443,
+    port: process.env.PORT || 8443,
     key:'./privatekey.pem',
     cert:'./certificate.pem',
   },
